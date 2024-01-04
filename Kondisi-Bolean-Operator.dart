c28bf1052dbac354f6d4018e4c8218f1bd1cdfca
delandapp <alihandronikolas@gmail.com>
2024-01-05 02:07:17 +0700

    kondisi operator dart

---
+++ Kondisi-Bolean-Operator.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  bool ab = !(a == b);
+  print(ab);
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+}
