8da62fb4f72877b1f27ff4e8d41edcadbf6c9c29
delandapp <alihandronikolas@gmail.com>
2024-01-04 22:59:45 +0700

    operator aritmatika dart

---
+++ operator-aritmatika.dart
@@ -0,0 +1,9 @@
+void main() {
+  int angkaPertama = 90;
+  int angkaKedua = 5;
+  print(angkaPertama + angkaKedua);
+  print(angkaPertama - angkaKedua);
+  print(angkaPertama * angkaKedua);
+  print(angkaPertama / angkaKedua);
+  print(angkaPertama % angkaKedua);
+}
