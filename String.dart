2410f6a232bd7051fdfbeaf530f6878b4b020f66
delandapp <alihandronikolas@gmail.com>
2024-01-05 01:39:38 +0700

    string dart

---
+++ String.dart
@@ -0,0 +1,8 @@
+void main() {
+  String a = 'abc'; // jadi string itu adalah int,huruf alfabet dimulai dari angka 97
+  print(a.codeUnits);
+
+  String firstName = 'Muhammad';
+  String lastName = 'Deland';
+  print(firstName + ' ' + lastName); // kita bisa menggabungkan string
+}
