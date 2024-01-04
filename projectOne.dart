da5f1dffcc2fb66f1da2fd7bc60f971fed9e2a21
delandapp <alihandronikolas@gmail.com>
2024-01-05 01:59:16 +0700

    update project simple v1

--- projectOne.dart
+++ projectOne.dart
@@ -7,5 +7,8 @@ void main() {
   var watchHour = 20.5;
   var hour = watchHour.toInt();
   var minutes = watchHour - hour;
-  print(' hello my name is $name \n I am working as $jobNow \n my age is $age \n I am currently working as $jobCurrent \n my marital status is $isMarried \n I have been working for ${hour} hour and ${minutes * 60} minutes');
+  var followers = 15000;
+  var followersR = followers % 1000;
+  var followersK = (followers - followersR) / 1000;
+  print(' hello my name is $name \n I am working as $jobNow \n my age is $age \n I am currently working as $jobCurrent \n my marital status is $isMarried \n I have been working for ${hour} hour and ${minutes * 60} minutes \n my followers is ${followersK.toInt()} K $followersR Followers');
 }
