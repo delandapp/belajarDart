6814696b579ab06a7cd7bced43fc25ecf082e83f
delandapp <alihandronikolas@gmail.com>
2024-01-05 15:42:18 +0700

    project three

---
+++ ProjectThree.dart
@@ -0,0 +1,16 @@
+void main() {
+  String sosialMedia = "youtube";
+  switch(sosialMedia) {
+    case "Facebook":
+      print("blue");
+      break;
+    case "Instagram":
+      print("red");
+      break;
+    case "Twitter":
+      print("black");
+      break;
+    default: 
+      print("sosial media unknown");
+  }
+}
