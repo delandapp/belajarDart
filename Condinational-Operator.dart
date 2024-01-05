e7b4505f179c75b11efc39cbfb128ab8e05ba45e
delandapp <alihandronikolas@gmail.com>
2024-01-05 15:30:32 +0700

    condinational operator

---
+++ Condinational-Operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  var likes = 100;
+  String message = (likes >= 100) ? 'Good Engagement!' : 'not Good Engagement';
+  // if(likes >= 100) {
+  //   message = 'Good Engagement!';
+  // } else {
+  //   message = 'not Good Engagement';
+  // }
+
+  print(message);
+}
